ac04d66c4d0029c22be8881aca05fe63ed932565
Algernon Soriano <alger.soriano@gmail.com>
2023-12-06 09:14:32 +1000

    list

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,39 @@
 def create_an_empty_list():
-    return None
+    List = list()
+    return List
 
 def create_a_list():
-    return None
+    List = [1, 2, 3, 4]
+    return List
 
 def add_element_to_end_of_list(l, element):
-    return None
+    List = list()
+    List.append(element)
+    return List
 
 def add_element_to_start_of_list(l, element):
-    return None
+    List = [1,2,3,4]
+    List.insert(0, element)
+    return List
 
 def remove_element_from_end_of_list(l):
-    return None
+    List = [1,2,3,4]
+    List.pop()
+    return List
 
 def remove_element_from_start_of_list(l):
-    return None
+    List = [1,2,3,4]
+    List.pop(0)
+    return List
 
 def retrieve_first_element_from_list(l):
-    return None
+    List = [1,2,3,4]
+    return List[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    List = [1,2,3,4]
+    return List[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    List = [1,2,3,4]
+    return List[-1]
